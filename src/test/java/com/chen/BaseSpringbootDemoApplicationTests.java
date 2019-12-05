ce70d2a65333d71e5f49c71383717a2018f5667d
chenjianwen <chenjianwen@coralglobal.cn>
2019-12-05 15:56:46 +0800

    mybatisplus测试用例

--- src/test/java/com/chen/BaseSpringbootDemoApplicationTests.java
+++ src/test/java/com/chen/BaseSpringbootDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.chen;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.chen.mysql.dao.ClassMapper;
 import com.chen.mysql.dao.StudentMapper;
 import com.chen.mysql.model.Student;
 import org.junit.jupiter.api.Test;
@@ -16,10 +18,54 @@ class BaseSpringbootDemoApplicationTests {
 
     @Autowired
     private StudentMapper studentMapper;
+    @Autowired
+    private ClassMapper classMapper;
 
+
+    /**
+     * 查询所有学生信息
+     */
     @Test
     public void test01(){
         List<Student> studentList = studentMapper.selectList(null);
         studentList.forEach(System.out::println);
+    }
+
+    /**
+     * 查询符合某条件的学生信息
+     */
+    @Test
+    public void test02(){
+        //List<Student> studentList = studentMapper.selectList(new QueryWrapper<Student>().eq("id",2));
+        List<Student> studentList2 = studentMapper.selectList(Wrappers.<Student>lambdaQuery().eq(Student::getId,1));
+        //studentList.forEach(System.out::println);
+        studentList2.forEach(System.out::println);
+    }
+
+    /**
+     * 查询符合某条件的学生数
+     */
+    @Test
+    public void test03(){
+        Integer count = studentMapper.selectCount(Wrappers.<Student>lambdaQuery().eq(Student::getClassName,"一班"));
+        System.out.println(count);
+    }
+
+    /**
+     * 根据条件查询某一具体对象
+     */
+    @Test
+    public void test04(){
+        Student student = studentMapper.selectOne(Wrappers.<Student>lambdaQuery().eq(Student::getId,1));
+        System.out.println(student);
+    }
+
+    /**
+     * 修改
+     */
+    @Test
+    public void test05(){
+        int value = studentMapper.update(new Student(),Wrappers.<Student>lambdaUpdate().set(Student::getName,"冥冥").eq(Student::getId,1));
+        System.out.println(value);
     }
 }
